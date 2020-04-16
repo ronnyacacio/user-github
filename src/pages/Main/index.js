4a6e289ca3183438a035b83fb2f66131d5277727
Ronny Acacio <ronnyacacio27@gmail.com>
2020-04-16 17:52:08 -0300

    style form

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
-import { View } from 'react-native';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import { Container, Form, Input, SubmitButton } from './styles';
 
 export default function Main() {
-  return <View />;
+  return (
+    <Container>
+      <Form>
+        <Input
+          autoCorrect={false}
+          autoCapitalize="none"
+          placeholder="Adicionar usuário"
+        />
+        <SubmitButton>
+          <Icon name="add" size={20} color="#FFF" />
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
 }
 
 Main.navigationOptions = {
